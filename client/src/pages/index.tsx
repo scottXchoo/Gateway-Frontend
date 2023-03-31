42ddf6eee9e356293b5e57892c5b0204b5882d99
scottXchoo <scottchoo@a41.io>
2023-03-31 15:00:37 +0900

    set initial setting & install Next.js, TypeScript

---
+++ client/src/pages/index.tsx
@@ -0,0 +1,23 @@
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import styles from "@/styles/Home.module.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Gateway | Rewards for Everything</title>
+        <meta
+          name="description"
+          content="Gateway on Archway, Rewards for Everything"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div>Home</div>
+    </>
+  );
+}
