b331b7d620782953260ae8c31a9107300841c5fc
scottXchoo <scottchoo@a41.io>
2023-03-31 15:51:28 +0900

    chore(client): tailwind.config.js

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,6 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,js, ts,jsx,tsx}"],
   theme: {
+    colors: {
+      orange: {
+        50: "#EB5B29",
+      },
+      gray: {
+        50: "#F2F2F2",
+        100: "#333333",
+      },
+    },
     extend: {},
   },
   plugins: [],
