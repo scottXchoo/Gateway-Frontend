4fde70450f65513eb2def51e207754b451b83bf0
scottXchoo <scottchoo@a41.io>
2023-04-10 17:53:53 +0900

    feat(client): ButtonLayout component

--- client/src/components/common/Button.tsx
+++ client/src/components/common/Button.tsx
@@ -1,2 +1,11 @@
+import tw from "tailwind-styled-components";
+
+export const ButtonLayout = tw.button`
+    border-[1px] border-white
+    rounded-[5px]
+    px-4 py-2
+    hover:bg-gradient-to-r from-orange-50 to-gray-150
+`;
+
 export const UploadButton = () => {};
 export const ActionButton = () => {};
