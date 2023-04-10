6275d8a5e3b70e781cb89e6a87c1ae6cd0dae89d
scottXchoo <scottchoo@a41.io>
2023-04-10 17:54:07 +0900

    feat(client): Header

--- client/src/pages/layout/Header.tsx
+++ client/src/pages/layout/Header.tsx
@@ -1,11 +1,26 @@
 import React from "react";
 import ComponentLayout from "./ComponentLayout";
+import Link from "next/link";
+import Image from "next/image";
+import { ButtonLayout } from "@/components/common/Button";
 
 const Header = () => {
   return (
     <ComponentLayout>
-      <div className="p-10">
-        <p className="text-orange-50 text-center">Header</p>
+      <div className="p-5 flex justify-between">
+        <Link href="/" className="flex items-center">
+          <Image src="/gateway_logo.png" alt="LOGO" width={50} height={50} />
+          <p className="text-white font-bold text-2xl ml-2">GATEWAY</p>
+        </Link>
+
+        <div className="flex items-center">
+          <ButtonLayout className="mr-3">
+            <p className="text-white">UPLOAD FILES</p>
+          </ButtonLayout>
+          <ButtonLayout>
+            <p className="text-white">CONNECT WALLET</p>
+          </ButtonLayout>
+        </div>
       </div>
     </ComponentLayout>
   );
