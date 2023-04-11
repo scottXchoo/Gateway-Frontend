a4daf88357df4012e5baf04fe545ea75ffd1db3f
scottXchoo <scottchoo@a41.io>
2023-04-11 16:54:42 +0900

    feat(client): ConnectWalletAlert component

---
+++ client/src/components/common/ConnectWalletAlert.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ConnectWalletAlert = () => {
+  return (
+    <div className=" border-2 border-white bg-orange-50 text-gray-100 p-7 rounded-[5px] w-1/3 mx-auto">
+      <p className="font-bold text-xl text-center">
+        🚀 Please Connect Wallet 🚀
+      </p>
+    </div>
+  );
+};
+
+export default ConnectWalletAlert;
