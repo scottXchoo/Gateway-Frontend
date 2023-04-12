1dccfa7c94096b63c38ce3f073e72c4e65f24ff2
scottXchoo <scottchoo@a41.io>
2023-04-12 20:53:14 +0900

    feat(client): upload files UI

--- client/src/components/common/UploadFileForm.tsx
+++ client/src/components/common/UploadFileForm.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
+import tw from "tailwind-styled-components";
 
 export interface FormData {
   title: string;
@@ -38,63 +39,81 @@ const UploadFileForm = ({ onSave, user = {} }: UploadFileFormProps) => {
 
   return (
     <form onSubmit={handleSubmit(handleSave)}>
-      <div>
-        <p>🌟 Title</p>
-        <input
-          {...register("title", {
-            required: true,
-            minLength: {
-              value: 3,
-              message: "Title must be at least 3 characters",
-            },
-          })}
-        />
-        {errors.title && "Error"}
-      </div>
-      <div>
-        <p>📝 Description</p>
-        <input
-          {...register("description", {
-            required: true,
-            minLength: {
-              value: 4,
-              message: "Description must be at least 4 characters",
-            },
-          })}
-        />
-        {errors.description && "Error"}
-      </div>
-      <div>
-        <p>🚀 Wallet Address</p>
-        <input
-          {...register("walletAddress", {
-            required: true,
-            pattern: {
-              value: /^0x[a-fA-F0-9]{40}$/,
-              message: "Wallet Address is invalid",
-            },
-          })}
-        />
-        {errors.walletAddress && "Error"}
-      </div>
-      <div>
-        <p>😺 Github Link</p>
-        <input
-          {...register("githubLink", {
-            required: true,
-            pattern: {
-              value: /^https?:\/\/github.com\/.+\/.+\/?$/,
-              message: "Github Link is invalid",
-            },
-          })}
-        />
-        {errors.githubLink && "Error"}
-      </div>
-      <div>
-        <button type="submit">SUBMIT</button>
+      <div className="text-left">
+        <div className="border-b border-gray-200/10 mb-3">
+          <h2 className=" text-lg font-semibold leading-7 text-gray-200 ">
+            🌟 Title
+          </h2>
+          <div className="my-2 flex rounded-[5px] shadow-lg sm:max-w-md focus:outline-none">
+            <input
+              placeholder="Write your title here"
+              className=" rounded-[5px] ring-1 ring-inset ring-gray-50/30 flex-auto border-0 py-1.5 pl-2 text-gray-150 placeholder:text-gray-50/30 focus:ring-0 sm:text-sm sm:leading-6"
+              {...register("title", {
+                required: true,
+                minLength: {
+                  value: 3,
+                  message: "Title must be at least 3 characters",
+                },
+              })}
+            />
+          </div>
+          {errors.title && (
+            <ErrorMessage>
+              <span className="font-semibold">Oh, snap!</span> Error Message
+            </ErrorMessage>
+          )}
+        </div>
+        <div>
+          <p>📝 Description</p>
+          <input
+            {...register("description", {
+              required: true,
+              minLength: {
+                value: 4,
+                message: "Description must be at least 4 characters",
+              },
+            })}
+          />
+          {errors.description && "Error"}
+        </div>
+        <div>
+          <p>🚀 Wallet Address</p>
+          <input
+            {...register("walletAddress", {
+              required: true,
+              pattern: {
+                value: /^0x[a-fA-F0-9]{40}$/,
+                message: "Wallet Address is invalid",
+              },
+            })}
+          />
+          {errors.walletAddress && "Error"}
+        </div>
+        <div>
+          <p>😺 Github Link</p>
+          <input
+            {...register("githubLink", {
+              required: true,
+              pattern: {
+                value: /^https?:\/\/github.com\/.+\/.+\/?$/,
+                message: "Github Link is invalid",
+              },
+            })}
+          />
+          {errors.githubLink && "Error"}
+        </div>
+        <div>
+          <button type="submit">SUBMIT</button>
+        </div>
       </div>
     </form>
   );
 };
 
 export default UploadFileForm;
+
+const ErrorMessage = tw.p`
+  text-red-600
+  text-sm
+  mb-2
+`;
--- client/src/pages/layout/DefaultLayout.tsx
+++ client/src/pages/layout/DefaultLayout.tsx
@@ -8,8 +8,7 @@ type LayoutProps = {
 };
 
 const Container = tw.div`
-    px-5
-    py-3
+    px-5 py-3
 `;
 
 const DefaultLayout = ({ children }: LayoutProps) => {
