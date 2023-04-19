7010b56346f7abde201c387d89c83cc5bc1952df
scottXchoo <scottchoo@a41.io>
2023-04-19 15:28:43 +0900

    feat(deps): install loadsh library

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    loader: "imgix",
-    path: "https://gateway-frontend.github.io/",
-  },
   reactStrictMode: true,
-  basePath: "/gateway-frontend",
 };
 
 module.exports = nextConfig;
