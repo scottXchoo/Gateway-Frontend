a0bccf4a741b7374463d2a741599faa0c161f6c1
scottXchoo <scottchoo@a41.io>
2023-04-19 11:07:29 +0900

    feat: deploy github-pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    loader: "imgix",
+    path: "https://gateway-frontend.github.io/",
+  },
   reactStrictMode: true,
-}
+  basePath: "/gateway-frontend",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
