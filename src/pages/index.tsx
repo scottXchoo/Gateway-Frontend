7363cced39612ec7360bcfac45675414bec72d3f
scottXchoo <scottchoo@a41.io>
2023-04-20 18:06:58 +0900

    feat: add favicon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -27,7 +27,7 @@ export default function Home() {
           content="Gateway on Archway, Rewards for Everything"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/gateway_favicon.ico" />
       </Head>
       <Hero />
       {isConnectWallet && <Main />}
